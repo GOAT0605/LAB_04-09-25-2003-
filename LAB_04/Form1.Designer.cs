c24adf31f47b510a8de2ce0d4a407aa677e1608b
Dell <Dell@DESKTOP-61HL79E>
2023-09-25 22:13:05 +0700

    Add project files.

---
+++ LAB_04/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace LAB_04
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAverageScore = new System.Windows.Forms.TextBox();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.cmbFacluty = new System.Windows.Forms.ComboBox();
+            this.dgvStudent = new System.Windows.Forms.DataGridView();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.BtnEdit = new System.Windows.Forms.Button();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.StdID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.STDName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Faculty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AverageScore = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(117, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(340, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quản lý thông tin Sinh Viên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Thông tin Sinh Viên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(65, 125);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mã số";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(65, 160);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Họ tên";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(65, 201);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Điểm TB";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(65, 240);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 20);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Khoa";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(142, 162);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(129, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtAverageScore
+            // 
+            this.txtAverageScore.Location = new System.Drawing.Point(141, 203);
+            this.txtAverageScore.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAverageScore.Name = "txtAverageScore";
+            this.txtAverageScore.Size = new System.Drawing.Size(118, 20);
+            this.txtAverageScore.TabIndex = 4;
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Location = new System.Drawing.Point(141, 127);
+            this.txtStudentID.Margin = new System.Windows.Forms.Padding(2);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(118, 20);
+            this.txtStudentID.TabIndex = 4;
+            // 
+            // cmbFacluty
+            // 
+            this.cmbFacluty.FormattingEnabled = true;
+            this.cmbFacluty.Location = new System.Drawing.Point(142, 240);
+            this.cmbFacluty.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbFacluty.Name = "cmbFacluty";
+            this.cmbFacluty.Size = new System.Drawing.Size(117, 21);
+            this.cmbFacluty.TabIndex = 5;
+            // 
+            // dgvStudent
+            // 
+            this.dgvStudent.AllowUserToAddRows = false;
+            this.dgvStudent.AllowUserToDeleteRows = false;
+            this.dgvStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudent.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.StdID,
+            this.STDName,
+            this.Faculty,
+            this.AverageScore});
+            this.dgvStudent.Location = new System.Drawing.Point(310, 115);
+            this.dgvStudent.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvStudent.Name = "dgvStudent";
+            this.dgvStudent.ReadOnly = true;
+            this.dgvStudent.RowHeadersWidth = 62;
+            this.dgvStudent.RowTemplate.Height = 28;
+            this.dgvStudent.Size = new System.Drawing.Size(462, 192);
+            this.dgvStudent.TabIndex = 6;
+            this.dgvStudent.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvStudent_CellClick);
+            this.dgvStudent.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvStudent_CellContentClick);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.Location = new System.Drawing.Point(243, 277);
+            this.BtnDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(53, 21);
+            this.BtnDelete.TabIndex = 7;
+            this.BtnDelete.Text = "Xóa";
+            this.BtnDelete.UseVisualStyleBackColor = true;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // BtnEdit
+            // 
+            this.BtnEdit.Location = new System.Drawing.Point(177, 277);
+            this.BtnEdit.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnEdit.Name = "BtnEdit";
+            this.BtnEdit.Size = new System.Drawing.Size(53, 21);
+            this.BtnEdit.TabIndex = 7;
+            this.BtnEdit.Text = "Sửa";
+            this.BtnEdit.UseVisualStyleBackColor = true;
+            this.BtnEdit.Click += new System.EventHandler(this.BtnEdit_Click);
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(106, 277);
+            this.BtnAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(53, 21);
+            this.BtnAdd.TabIndex = 7;
+            this.BtnAdd.Text = "Thêm";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(571, 318);
+            this.BtnExit.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(59, 23);
+            this.BtnExit.TabIndex = 8;
+            this.BtnExit.Text = "Thoát";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // StdID
+            // 
+            this.StdID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.StdID.HeaderText = "Mã SV";
+            this.StdID.MinimumWidth = 8;
+            this.StdID.Name = "StdID";
+            this.StdID.ReadOnly = true;
+            // 
+            // STDName
+            // 
+            this.STDName.HeaderText = "Tên SV";
+            this.STDName.MinimumWidth = 8;
+            this.STDName.Name = "STDName";
+            this.STDName.ReadOnly = true;
+            this.STDName.Width = 150;
+            // 
+            // Faculty
+            // 
+            this.Faculty.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Faculty.HeaderText = "Khoa";
+            this.Faculty.MinimumWidth = 8;
+            this.Faculty.Name = "Faculty";
+            this.Faculty.ReadOnly = true;
+            // 
+            // AverageScore
+            // 
+            this.AverageScore.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.AverageScore.HeaderText = "DTB";
+            this.AverageScore.MinimumWidth = 8;
+            this.AverageScore.Name = "AverageScore";
+            this.AverageScore.ReadOnly = true;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 398);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnEdit);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.dgvStudent);
+            this.Controls.Add(this.cmbFacluty);
+            this.Controls.Add(this.txtStudentID);
+            this.Controls.Add(this.txtAverageScore);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form";
+            this.Text = "QLSV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAverageScore;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.ComboBox cmbFacluty;
+        private System.Windows.Forms.DataGridView dgvStudent;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.Button BtnEdit;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StdID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn STDName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Faculty;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AverageScore;
+    }
+}
+
